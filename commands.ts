0e953616c6649eeb80f8add94e0b013e03bf1f1f
oscarheimdahl <o.heimdahl@gmail.com>
2025-02-22 16:14:19 +0100

    Add functionality

---
+++ commands.ts
@@ -0,0 +1,31 @@
+const seperator = "|||";
+export const forEachRefCmd = {
+  seperator,
+  cmd: new Deno.Command("git", {
+    args: [
+      "for-each-ref",
+      "--sort=-committerdate",
+      "refs/heads",
+      `--format=${seperator}%(refname:short)${seperator}%(committerdate:relative)`,
+    ],
+  }),
+};
+
+export const checkoutCmd = (branch: string) =>
+  new Deno.Command("git", {
+    args: ["checkout", branch],
+  });
+
+export const currentBranchCmd = new Deno.Command("git", {
+  args: ["branch", "--show-current"],
+});
+
+export const handleGitError = (
+  cmdName: string,
+  stderr: Uint8Array<ArrayBuffer>
+) => {
+  console.log(`An error occurred running 'git ${cmdName}':\n`);
+  console.log(new TextDecoder().decode(stderr));
+  console.log("Exiting...");
+  Deno.exit(1);
+};
--- main.ts
+++ main.ts
@@ -1,8 +1,57 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import * as inquirer from "@inquirer/prompts";
+import {
+  forEachRefCmd,
+  checkoutCmd,
+  handleGitError,
+  currentBranchCmd,
+} from "./commands.ts";
+
+const decoder = new TextDecoder();
+
+const currentBranchOutput = await currentBranchCmd.output();
+
+if (!currentBranchOutput.success) {
+  handleGitError("branch --show-current", currentBranchOutput.stderr);
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+const currentBranch = decoder.decode(currentBranchOutput.stdout).trim();
+
+const output = await forEachRefCmd.cmd.output();
+
+if (!output.success) {
+  handleGitError("for-each-ref", output.stderr);
 }
+
+const refRows = decoder.decode(output.stdout).split("\n").slice(0, -1);
+
+const refRowsSplit = refRows.map((row) =>
+  row.split(forEachRefCmd.seperator).slice(1)
+);
+
+const selectedBranch = await inquirer.select({
+  message: "Select branch:",
+  choices: [...refRowsSplit].map((row) => ({
+    name: `${row[0]} - ${row[1]}`,
+    value: row[0],
+  })),
+});
+
+if (
+  typeof selectedBranch !== "string" ||
+  (typeof selectedBranch === "string" && selectedBranch.length < 1)
+) {
+  console.log("An invalid branch was selected");
+  Deno.exit(1);
+}
+
+const cmd = checkoutCmd(selectedBranch);
+
+const output2 = await cmd.output();
+
+if (!output2.success) {
+  handleGitError("checkout", output.stderr);
+}
+
+console.log(`${currentBranch} -> ${selectedBranch}`);
+
+Deno.exit(0);
